7ef30d94ec00fc15ca3eb73ee7bbf5241cd4cdd0
Dominik Mikiewicz <dominik@cartomatic.pl>
2019-04-08 17:32:56 +0200

    Add api endpoint info controller

---
+++ MapHive.Api.Core/Controllers/EndpointInfoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cartomatic.Utils.Email;
+using MapHive.Core;
+using MapHive.Core.Api.ApiControllers;
+using MapHive.Core.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MapHive.Api.Core.Controllers
+{
+    /// <summary>
+    /// Endpoint info APIs
+    /// </summary>
+    [Route("endpointinfo")]
+    public class EndpointInfoController : DbCtxController<MapHiveDbContext>
+    {
+        /// <summary>
+        /// Returns information on the deployed api version
+        /// </summary>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("version")]
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetVersionInfo()
+        {
+            try
+            {
+                return Ok(Cartomatic.Utils.Git.GetRepoVersion());
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
+    }
+}
