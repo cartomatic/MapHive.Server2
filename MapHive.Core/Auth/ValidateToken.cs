68933f55bad7a202aaf0af93c728c01700705340
Dominik Mikiewicz <dominik@cartomatic.pl>
2019-05-01 12:46:58 +0200

    Fix token validation logic so it uses new token instrospection endpoint

--- MapHive.Core/Auth/ValidateToken.cs
+++ MapHive.Core/Auth/ValidateToken.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using IdentityModel.Client;
+using IdentityServer4.Models;
 using MapHive.Core.IdentityServer.SerializableConfig;
 using RestSharp;
 
@@ -13,28 +16,35 @@ namespace MapHive.Core
     {
         public static async Task<AuthOutput> ValidateTokenAsync(string accessToken)
         {
-            var idSrvTokenClientOpts = IdSrvTokenClientOpts.InitDefault();
+            var idSrvTokenBearerOpts = IdSrvTokenBearerOpts.InitDefault();
 
-            //GET /connect/accesstokenvalidation?token=<token>
+            //using IdentityModel for token introspection
+            //it provides anice functionality extension set over http client
 
-            var client = new RestClient($"{idSrvTokenClientOpts.Authority}/connect");
-            var request = new RestRequest("accesstokenvalidation", Method.GET);
-            request.AddQueryParameter("token", accessToken);
-
-            var response = await client.ExecuteTaskAsync<Dictionary<string, string>>(request);
-
-            return new AuthOutput
+            using (var client = new HttpClient())
             {
-                Success = response.StatusCode == HttpStatusCode.OK,
-                AccessToken = accessToken,
+                var response = await client.IntrospectTokenAsync(new TokenIntrospectionRequest
+                {
+                    Address = $"{idSrvTokenBearerOpts.Authority}/connect/introspect",
+                    ClientId = idSrvTokenBearerOpts.ApiName,
+                    ClientSecret = idSrvTokenBearerOpts.ApiSecret,
 
-                //Note: accesstokenvalidation returns expiration in seconds since epoch.
-                //Code below should give time in UTC 
-                AccessTokenExpirationTimeUtc =
-                    response.StatusCode == HttpStatusCode.OK
-                    ? new DateTime(1970, 01, 01, 0, 0, 0, DateTimeKind.Utc).AddSeconds(response.Data.ContainsKey("exp") ? long.Parse(response.Data["exp"]) : 0)
-                    : (DateTime?)null
-            };
+                    Token = accessToken
+                });
+
+                return new AuthOutput
+                {
+                    Success = !response.IsError && response.IsActive,
+                    AccessToken = accessToken,
+
+                    //Note: expiration in seconds since epoch.
+                    //Code below should give time in UTC 
+                    AccessTokenExpirationTimeUtc = !response.IsError && response.IsActive
+                        ? new DateTime(1970, 01, 01, 0, 0, 0, DateTimeKind.Utc).AddSeconds(
+                            long.Parse(response.Claims.FirstOrDefault(x => x.Type == "exp")?.Value ?? "0"))
+                        : (DateTime?) null
+                };
+            }
         }
     }
 }
