f32bb942db085bb07a18dff3cede098e28f6a2b9
Dominik Mikiewicz <dominik@cartomatic.pl>
2019-05-07 18:49:18 +0200

    change the headers passed through by the api caller

--- MapHive.Core.Api/ApiControllers/BaseController/RequestHeaders.cs
+++ MapHive.Core.Api/ApiControllers/BaseController/RequestHeaders.cs
@@ -29,8 +29,22 @@ namespace MapHive.Core.Api.ApiControllers
             return authToken;
         }
 
-     
-        
+
+        private string[] _headersToIgnoreWhenTransferring = new[]
+        {
+            //ignore auth; it is transferred separately
+            "authorization",
+            //always avoid passing host header as it means when DNS resolves url to ip and request hits the sever it will resolve back to the domain thie request has originated from - the initial api
+            //it would be pretty much ok, if the endpoints used different web servers or machines, but will casue problems when deployed from a single IIS!
+            "host",
+
+            //ignore content type as it may have been compressed on input and this call will send just json serialized data
+            "content-type",
+
+            //ignore content length! After all when new content is added for POST/PUT it will be worked out appropriately during data serialization and / or compression!
+            //also for scenarios, when original request that initiated another request being performed in the background and being of different type (be it whatever, GET, PUT) and different length than the initiator, passing an invalid content length will make RestSharp request take ages to complete (likely because either client or server will be waiting for data me thinks ;)
+            "content-length"
+        };
 
         /// <summary>
         /// Transfers request headers
@@ -43,26 +57,15 @@ namespace MapHive.Core.Api.ApiControllers
         {
             foreach (var hdr in Request.Headers)
             {
-                //ignore auth; it is transferred separately
-                if(hdr.Key.ToLower() == "authorization")
+                if (_headersToIgnoreWhenTransferring.Contains(hdr.Key.ToLower()))
                     continue;
-
-                //always avoid passing host header as it means when DNS resolves url to ip and request hits the sever it will resolve back to the domain thie request has originated from - the initial api
-                //it would be pretty much ok, if the endpoints used different web servers or machines, but will casue problems when deployed from a single IIS!
-                if (hdr.Key.ToLower() == "host")
-                    continue;
-
-                //ignore content type as it may have been compressed on input
-                if (hdr.Key.ToLower() == "content-type")
-                    continue;
-                //TODO-support re-compressing content.
 
                 //assume that custom headers overwrite whatever are the incoming headers
                 //this is done outside of this method
                 if (customHdrs?.ContainsKey(hdr.Key) == true)
                     continue;
 
-                //should mh headers be transferred
+                //should mh headers be transferred?
                 var mhHdrs = WebClientConfiguration.GetMhHeaders();
                 if(mhHdrs.Contains(hdr.Key) && !tranferMhHdrs)
                     continue;
