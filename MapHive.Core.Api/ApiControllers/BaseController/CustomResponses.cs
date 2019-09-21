2362ed39ed7fa578ce3e50ce6763ac778afd35cf
Dominik Mikiewicz <dominik@cartomatic.pl>
2019-09-21 09:35:34 +0200

    Update CustomResponses.cs

--- MapHive.Core.Api/ApiControllers/BaseController/CustomResponses.cs
+++ MapHive.Core.Api/ApiControllers/BaseController/CustomResponses.cs
@@ -1,16 +1,21 @@
-﻿using MapHive.Core.DataModel.Validation;
+﻿using System.Net;
+using MapHive.Core.DataModel.Validation;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MapHive.Core.Api.ApiControllers
 {
     public abstract partial class BaseController
     {
         /// <summary>
-        /// Return 400 with no permission message error
+        /// Return 403 with no permission message error
         /// </summary>
         /// <returns></returns>
-        //protected static NegotiatedContentResult<object> NoPermissionError()
-        //{
-        //    throw MapHive.Core.DataModel.Validation.Utils.GenerateValidationFailedException("role", ValidationErrors.NoPermission);
-        //}
+        protected static IActionResult NoPermissionError()
+        {
+            return new ObjectResult(ValidationErrors.NoPermission)
+            {
+                StatusCode = (int)HttpStatusCode.Forbidden
+            };
+        }
     }
 }
