c9fab3723605264bb8b458448803039536883cbc
Dominik Mikiewicz <dominik@cartomatic.pl>
2019-10-04 18:37:33 +0200

    MapHive.Api.Core initially starts

--- MapHive.Core.Api/ApiControllers/BaseController/CallExternalRestApi.cs
+++ MapHive.Core.Api/ApiControllers/BaseController/CallExternalRestApi.cs
@@ -9,7 +9,6 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using BrotliSharpLib;
 using Cartomatic.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
