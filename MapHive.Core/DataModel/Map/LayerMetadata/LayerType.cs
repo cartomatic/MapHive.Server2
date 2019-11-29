4d3b5e0900d1e43dd0521db2cf3613535e85b8d6
Dominik Mikiewicz <dominik@cartomatic.pl>
2019-11-29 19:06:32 +0100

    Add some custom types to ayer type enum

--- MapHive.Core/DataModel/Map/LayerMetadata/LayerType.cs
+++ MapHive.Core/DataModel/Map/LayerMetadata/LayerType.cs
@@ -6,13 +6,25 @@ namespace MapHive.Core.DataModel.Map
 {
     public enum LayerType
     {
-        Custom, //not used anywhere really...
         SHP = 1,
         WMS,
         WFS,
         GeoJSON,
         JSON,
-        CSV
+        CSV,
+
+        //some custom holders
+        CUSTOM01 = 101,
+        CUSTOM02 = 102,
+        CUSTOM03 = 103,
+        CUSTOM04 = 104,
+        CUSTOM05 = 105,
+        CUSTOM06 = 106,
+        CUSTOM07 = 107,
+        CUSTOM08 = 108,
+        CUSTOM09 = 109,
+        CUSTOM10 = 110,
+
     }
 }
     
