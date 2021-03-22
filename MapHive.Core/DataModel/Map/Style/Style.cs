7c92ed5688b09fb6a6d8c7cec7b4f9bc30244844
Dominik Mikiewicz <dominik@cartomatic.pl>
2021-03-22 06:39:36 +0100

    extend layer style model

--- MapHive.Core/DataModel/Map/Style/Style.cs
+++ MapHive.Core/DataModel/Map/Style/Style.cs
@@ -44,6 +44,114 @@ namespace MapHive.Core.DataModel.Map
         public double? PolyStrokeWidth { get; set; }
 
         /// <summary>
+        /// Whether or not should label polys
+        /// </summary>
+        public bool? PolyLabels { get; set; }
+
+        /// <summary>
+        /// field used to label polys
+        /// </summary>
+        public string PolyLabelField { get; set; }
+
+        /// <summary>
+        /// When to start showing poly labels when zooming in and stop showing poly labels when zooming out
+        /// </summary>
+        public int? PolyLabelVisibilityScaleMin { get; set; }
+
+        /// <summary>
+        /// when to stop showing poly labels when zooming in and start showing poly labels when zooming out
+        /// </summary>
+        public int? PolyLabelVisibilityScaleMax { get; set; }
+
+        /// <summary>
+        /// Whether or not poly labels should be decluttered
+        /// </summary>
+        public bool? PolyLabelDeclutter { get; set; }
+
+        /// <summary>
+        /// Poly label wrapping: hide, normal, shorten, wrap
+        /// </summary>
+        public string PolyLabelWrap { get; set; }
+
+        /// <summary>
+        /// Poly label align: center, end, left, right, start
+        /// </summary>
+        public string PolyLabelAlign { get; set; }
+
+        /// <summary>
+        /// Poly label baseline: alphabetic, bottom, hanging, ideographic, middle, top
+        /// </summary>
+        public string PolyLabelBaseLine { get; set; }
+
+        /// <summary>
+        /// Font used to label a poly
+        /// </summary>
+        public string PolyLabelFont { get; set; }
+
+        /// <summary>
+        /// Rotation of a poly label
+        /// </summary>
+        public int? PolyLabelRotation { get; set; }
+
+        /// <summary>
+        /// Poly label style: Normal, Bold, Italic, Bold Italic
+        /// </summary>
+        public string PolyLabelFontStyle { get; set; }
+
+        /// <summary>
+        /// Placement of a poly label: point, line
+        /// </summary>
+        public string PolyLabelPlacement { get; set; }
+
+        /// <summary>
+        /// Max angle a label can be rotated when being rendered along an object
+        /// </summary>
+        public int? PolyLabelMaxAngle { get; set; }
+
+        /// <summary>
+        /// Whether or not a poly label can overflow
+        /// </summary>
+        public bool PolyLabelOverflow { get; set; }
+
+        /// <summary>
+        /// Size of a poly label
+        /// </summary>
+        public int? PolyLabelSize { get; set; }
+
+        /// <summary>
+        /// Line height for poly labels
+        /// </summary>
+        public decimal? PolyLabelLineHeight { get; set; }
+
+        /// <summary>
+        /// Poly label x offset
+        /// </summary>
+        public int? PolyLabelOffsetX { get; set; }
+
+        /// <summary>
+        /// Poly label y offset
+        /// </summary>
+        public int? PolyLabelOffsetY { get; set; }
+
+        /// <summary>
+        /// Poly label color
+        /// </summary>
+        public string PolyLabelColor { get; set; }
+
+        /// <summary>
+        /// Poly label outline color
+        /// </summary>
+        public string PolyLabelOutlineColor { get; set; }
+
+        /// <summary>
+        /// Poly label outline width
+        /// </summary>
+        public int? PolyLabelOutlineWidth { get; set; }
+
+
+
+
+        /// <summary>
         /// Whether or not polyline style should be applied
         /// </summary>
         public bool? Line { get; set; }
@@ -57,6 +165,113 @@ namespace MapHive.Core.DataModel.Map
         /// Width of a stroke
         /// </summary>
         public double? LineStrokeWidth { get; set; }
+
+        /// <summary>
+        /// Whether or not should label lines
+        /// </summary>
+        public bool? LineLabels { get; set; }
+
+        /// <summary>
+        /// field used to label lines
+        /// </summary>
+        public string LineLabelField { get; set; }
+
+        /// <summary>
+        /// When to start showing line labels when zooming in and stop showing line labels when zooming out
+        /// </summary>
+        public int? LineLabelVisibilityScaleMin { get; set; }
+
+        /// <summary>
+        /// when to stop showing line labels when zooming in and start showing line labels when zooming out
+        /// </summary>
+        public int? LineLabelVisibilityScaleMax { get; set; }
+
+        /// <summary>
+        /// Whether or not line labels should be decluttered
+        /// </summary>
+        public bool? LineLabelDeclutter { get; set; }
+
+        /// <summary>
+        /// Line label wrapping: hide, normal, shorten, wrap
+        /// </summary>
+        public string LineLabelWrap { get; set; }
+
+        /// <summary>
+        /// Line label align: center, end, left, right, start
+        /// </summary>
+        public string LineLabelAlign { get; set; }
+
+        /// <summary>
+        /// Line label baseline: alphabetic, bottom, hanging, ideographic, middle, top
+        /// </summary>
+        public string LineLabelBaseLine { get; set; }
+
+        /// <summary>
+        /// Font used to label a line
+        /// </summary>
+        public string LineLabelFont { get; set; }
+
+        /// <summary>
+        /// Rotation of a line label
+        /// </summary>
+        public int? LineLabelRotation { get; set; }
+
+        /// <summary>
+        /// Line label style: Normal, Bold, Italic, Bold Italic
+        /// </summary>
+        public string LineLabelFontStyle { get; set; }
+
+        /// <summary>
+        /// Placement of a line label: point, line
+        /// </summary>
+        public string LineLabelPlacement { get; set; }
+
+        /// <summary>
+        /// Max angle a line label can be rotated when being rendered along an object
+        /// </summary>
+        public int? LineLabelMaxAngle { get; set; }
+
+        /// <summary>
+        /// Whether or not a line label can overflow
+        /// </summary>
+        public bool LineLabelOverflow { get; set; }
+
+        /// <summary>
+        /// Size of a line label
+        /// </summary>
+        public int? LineLabelSize { get; set; }
+
+        /// <summary>
+        /// Line height for line labels
+        /// </summary>
+        public decimal? LineLabelLineHeight { get; set; }
+
+        /// <summary>
+        /// Line label x offset
+        /// </summary>
+        public int? LineLabelOffsetX { get; set; }
+
+        /// <summary>
+        /// Line label y offset
+        /// </summary>
+        public int? LineLabelOffsetY { get; set; }
+
+        /// <summary>
+        /// line label color
+        /// </summary>
+        public string LineLabelColor { get; set; }
+
+        /// <summary>
+        /// Line label outline color
+        /// </summary>
+        public string LineLabelOutlineColor { get; set; }
+
+        /// <summary>
+        /// Line label outline width
+        /// </summary>
+        public int? LineLabelOutlineWidth { get; set; }
+
+
 
         /// <summary>
         /// Whether or not point style should be applied
@@ -107,6 +322,11 @@ namespace MapHive.Core.DataModel.Map
         /// when to stop showing point labels when zooming in and start showing point labels when zooming out
         /// </summary>
         public int? PointLabelVisibilityScaleMax { get; set; }
+
+        /// <summary>
+        /// Whether or not labels should be decluttered
+        /// </summary>
+        public bool? PointLabelDeclutter { get; set; }
 
         /// <summary>
         /// point label wrapping: hide, normal, shorten, wrap
@@ -187,11 +407,6 @@ namespace MapHive.Core.DataModel.Map
         /// point label outline width
         /// </summary>
         public int? PointLabelOutlineWidth { get; set; }
-
-        ///// <summary>
-        ///// Whether or not labels should be declutterred
-        ///// </summary>
-        //public bool? PointLabelDeclutter { get; set; }
 
     }
 }
